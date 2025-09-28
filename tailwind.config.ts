4c2f24aa03e717710802a4aa182a6651850f47d5
FXND-DE <fxnd@mac.com>
2025-09-28 16:27:25 +0200

    Sync latest local changes for deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
   './lib/**/*.{js,ts,jsx,tsx}',
 ],
   theme: {
+        fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
+        'base': ['1rem', { lineHeight: '1.5rem' }],
+        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      },
     extend: {
       colors: {
         'nh-beige': '#f5f7f0',
@@ -20,15 +29,7 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-quicksand)', 'sans-serif'],
       },
-      fontSize: {
-        'xs': ['0.75rem', { lineHeight: '1rem' }],
-        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
-        'base': ['1rem', { lineHeight: '1.5rem' }],
-        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
-        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
-        '2xl': ['1.5rem', { lineHeight: '2rem' }],
-        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-      },
+
     },
     // ❗️Hierhin gehört `screens`, nicht in `extend`
     screens: {
